fd9dda4d70955f6956e29021e98379829ee159c6
Taneya <twil609@aucklanduni.ac.nz>
2022-03-16 16:15:39 +1000

    airplane class complete

---
+++ src/ictgradschool/airplane/airplane.java
@@ -0,0 +1,43 @@
+package ictgradschool.airplane;
+
+public class airplane {
+    private String colour;
+    private double ticketPrice;
+    private boolean ticketAvailable;
+
+    public airplane() {
+        colour = "white";
+        ticketPrice = 123.45;
+        ticketAvailable = true;
+    }
+
+    public airplane(String colour, double ticketPrice, boolean ticketAvailable) {
+        this.colour = colour;
+        this.ticketPrice = ticketPrice;
+        this.ticketAvailable = ticketAvailable;
+    }
+
+    public String checkTicketAvailable() {
+        if (ticketAvailable) {
+            return "Ticket still available";
+        }
+        else {
+            return "Ticket no longer available";
+        }
+    }
+
+    public double getTicketPrice() {
+        return this.ticketPrice;
+    }
+
+    public void takeOff() {
+        System.out.println("The " + colour + "plane is ready for take off!");
+    }
+
+    public void setTicketAvailable(boolean updatedAvailability) {
+        ticketAvailable = updatedAvailability;
+    }
+}
+
+
+
